d1eb5001594a27f2a6787cba8aa2243beef547d3
MIXprog <56017422+MIXprog@users.noreply.github.com>
2022-05-17 18:31:47 +0200

    added interface and change controller
    HomeController - Details(int modelId) added
    ICarRepository interface added
    SQLCarRepository added (class impelements ICarRepository)

--- Car Showroom/Controllers/HomeController.cs
+++ Car Showroom/Controllers/HomeController.cs
@@ -6,23 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Car_Showroom.Models;
+using Car_Showroom.DataAccess;
 
 namespace Car_Showroom.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICarRepository carRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICarRepository carRepository)
         {
             _logger = logger;
+            carRepository = carRepository;
         }
 
         public IActionResult Index()
         {
             return View();
         }
-
+        public async Task<IActionResult> Details(int modelId)
+        {
+            Car car = await carRepository.GetCar(modelId);
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
---
+++ Car Showroom/DataAccess/SQLCarRepository.cs
@@ -0,0 +1,34 @@
+﻿using Car_Showroom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Car_Showroom.DataAccess
+{
+    public class SQLCarRepository : ICarRepository
+    {
+        private readonly CarDealershipsContext dbContext;
+
+        public SQLCarRepository(CarDealershipsContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Car> GetCar(int modelId)
+        {
+           var car = await dbContext.Car.FindAsync(modelId);
+            return car;
+        }
+
+      
+
+        public List<Model> GetModels()
+        {
+            List<Model> modelList = dbContext.Model.ToList();
+            return modelList;
+        }
+
+     
+    }
+}
