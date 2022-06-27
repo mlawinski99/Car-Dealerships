0cb9d93993403fdb3541a761bbe729ffcd825bae
DudaRobert <duda_robert@outlook.com>
2022-06-27 16:59:27 +0200

    Admin view folder refactorization

---
+++ CarDealershipsManagementSystem/Views/Admin/AdminNav.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CarDealershipsManagementSystem.Views.Admin
+{
+    public static class AdminNav
+    {
+        public static string? IsActive(ViewContext viewContext, string linkId)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string;
+            return 
+                (activePage != null && activePage.Contains(linkId)) 
+                ? "active" 
+                : null;
+        }
+    }
+}
